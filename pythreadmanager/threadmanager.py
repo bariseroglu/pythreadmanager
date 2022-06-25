c50273eb12055080da8f858d0e48e226efde91c4
Barış Ege Eroğlu <eroglubarisege@gmail.com>
2022-06-25 16:04:09 +0300

    once method added

--- pythreadmanager/threadmanager.py
+++ pythreadmanager/threadmanager.py
@@ -1,6 +1,17 @@
 from threading import Thread
 import time
 
+class Once:
+
+    def __init__(self, func, args, daemon=False):
+        self.func = func
+        self.args = args
+        self.terminator = False
+        self.thread = Thread(target=self.func, args=args, daemon=daemon)
+        self.thread.start()
+
+    def wait(self):
+        self.thread.join()
 
 class Loop:
 
@@ -31,7 +42,6 @@ class Timeout:
         self.func = func
         self.args = args
         self.timeout = timeout
-        self.terminator = False
         self.thread = Thread(target=self.timeoutFunc, daemon=daemon)
         self.thread.start()
 
